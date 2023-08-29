bfe893eca02e7dcb47f0ebd0347669716d7bf389
CAI WENZHE <caiwenzhe2021@gmail.com>
2023-08-29 21:08:43 +0800

    update

--- grafana/run.sh
+++ grafana/run.sh
@@ -2,9 +2,9 @@
 file_path=`readlink -f $0`
 dir_path=`dirname $file_path`
 
-
 sudo docker run -d \
     --name=grafana \
+    --user root \
     --restart unless-stopped \
     -p 3000:3000 \
     -v /etc/localtime:/etc/localtime \
