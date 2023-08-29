fa7845137c7a13db079c3963a64f7504c9a238d3
CAI WENZHE <caiwenzhe2021@gmail.com>
2023-08-29 20:14:58 +0800

    prometheus

---
+++ prometheus/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+file_path=`readlink -f $0`
+dir_path=`dirname $file_path`
+
+sudo docker run --name prometheus \
+    -v $dir_path/stack-data:/opt/bitnami/prometheus/data \
+    -v $dir_path/prometheus.yml:/opt/bitnami/prometheus/conf/prometheus.yml \
+    -p 9090:9090 \
+    bitnami/prometheus:latest
